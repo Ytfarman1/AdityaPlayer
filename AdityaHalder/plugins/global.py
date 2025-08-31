aab4a829db84e52d31229d7c2714a64f7de900c1
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2025-08-31 15:08:59 +0530

    Update global.py

--- AdityaHalder/plugins/global.py
+++ AdityaHalder/plugins/global.py
@@ -8,20 +8,8 @@ from pyrogram.types import ChatMemberUpdated
 @bot.on_chat_member_updated()
 async def bot_added_to_group(client, event: ChatMemberUpdated):
     chat_id = event.chat.id
-    
-    if event.new_chat_member and event.new_chat_member.user.id == bot.id:
-        await add_served_chat(chat_id)
+    await add_served_chat(chat_id)
         
-    if event.chat.username:
-        chat_link = f"https://t.me/{event.chat.username}"
-        console.chat_links[chat_id] = chat_link
-    else:
-        try:
-            chat_link = await bot.export_chat_invite_link(chat_id)
-            console.chat_links[chat_id] = chat_link
-        except Exception:
-            pass
-
     if event.chat.id not in console.chat_admins:
         console.chat_admins[chat_id] = {}
     try:
